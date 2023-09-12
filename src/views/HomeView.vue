46579e635bd066b28a853f4ddecd520e7a2915fd
MuhammadHafijur <85216180+MuhammadHafijur@users.noreply.github.com>
2023-09-12 18:12:08 +0600

    :sparkles: feat: Transition component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <TheToast v-if="showToast" />
     <TheTodos @badValue="triggerToast" />
+    <Transition name="fade">
+      <div v-if="showP">Hello, people!</div>
+    </Transition>
+
+    <button @click="showP = !showP">toggle</button>
   </div>
 </template>
 
@@ -14,17 +19,38 @@ export default {
   components: { TheToast, TheTodos },
   setup() {
     const showToast = ref(false)
+    const showP = ref(false)
 
     const triggerToast = () => {
       showToast.value = true;
       setTimeout(() => showToast.value = false, 3000)
     }
 
-    return { showToast, triggerToast }
+    return { showToast, triggerToast, showP }
   }
 }
 </script>
 
 <style>
+.fade-enter-from{
+  opacity: 0;
+
+}
+.fade-enter-to{
+  opacity: 1;
+}
+.fade-enter-active{
+  transition: all 2s ease;
+}
+.fade-leave-from{
+  opacity: 1;
+}
+.fade-leave-to{
+  opacity: 0;
+}
+.fade-leave-active{
+  transition: all 2s ease;
+}
+
 
 </style>
