874131e31ce74c47fb11adeded18c184a3d4f9fc
MuhammadHafijur <85216180+MuhammadHafijur@users.noreply.github.com>
2023-09-12 16:33:28 +0600

    :tada: setup: Initial setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <TheToast v-if="showToast" />
+    <TheTodos @badValue="triggerToast" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import TheToast from '../components/TheToast.vue'
+import TheTodos from '../components/TheTodos.vue'
+
+export default {
+  components: { TheToast, TheTodos },
+  setup() {
+    const showToast = ref(false)
+
+    const triggerToast = () => {
+      showToast.value = true;
+      setTimeout(() => showToast.value = false, 3000)
+    }
+
+    return { showToast, triggerToast }
+  }
+}
+</script>
+
+<style>
+
+</style>
